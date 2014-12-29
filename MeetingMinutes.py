ce3efab0b7217360e1580c7fd0a2033ed29499d5
Aitor Carlos Urrutia Aranburu <contact@aitorcurrutia.com>
2014-12-29 23:32:43 +0100

    Parse markdown text.

--- MeetingMinutes.py
+++ MeetingMinutes.py
@@ -1,5 +1,10 @@
 import sublime, sublime_plugin
+from .mistune import markdown
 
 class CreateMinuteCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.run_command("markdown_preview_select", {"target":"save"})
+		region = sublime.Region(0, self.view.size())
+		md_source = self.view.substr(region)
+		md_source.encode(encoding='UTF-8',errors='strict')
+		html_source = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>' + markdown(md_source) + '</body></html>'
+		print(html_source)

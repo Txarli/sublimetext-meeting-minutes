34369635a22bf05abbabe47e708a2ed80db258e5
Aitor Carlos Urrutia Aranburu <contact@aitorcurrutia.com>
2014-12-30 00:09:35 +0100

    Save the created html in a HTML file.

--- MeetingMinutes.py
+++ MeetingMinutes.py
@@ -1,5 +1,10 @@
 import sublime, sublime_plugin
+import os
+import re
+from subprocess import call
+
 from .mistune import markdown
+
 
 class CreateMinuteCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -7,4 +12,12 @@ class CreateMinuteCommand(sublime_plugin.TextCommand):
 		md_source = self.view.substr(region)
 		md_source.encode(encoding='UTF-8',errors='strict')
 		html_source = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>' + markdown(md_source) + '</body></html>'
-		print(html_source)
+		
+		file_name = self.view.file_name()
+		html_file, extension = os.path.splitext(file_name)
+		html_file += ".html"
+		with open(html_file, 'w+') as file_:
+			file_.write(html_source)
+
+		print(file_name)
+		print(html_file)

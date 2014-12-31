8bce9128c610da72a5b02e081a8b46156b8498dc
Aitor Carlos Urrutia Aranburu <contact@aitorcurrutia.com>
2014-12-31 11:59:35 +0100

    Create change_extension function.

--- MeetingMinutes.py
+++ MeetingMinutes.py
@@ -1,7 +1,6 @@
 import sublime, sublime_plugin
 import os
 import re
-from subprocess import call
 
 from .mistune import markdown
 
@@ -14,10 +13,17 @@ class CreateMinuteCommand(sublime_plugin.TextCommand):
 		html_source = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>' + markdown(md_source) + '</body></html>'
 		
 		file_name = self.view.file_name()
-		html_file, extension = os.path.splitext(file_name)
-		html_file += ".html"
+		html_file = self.change_extension(file_name, ".html")
 		with open(html_file, 'w+') as file_:
 			file_.write(html_source)
 
 		print(file_name)
 		print(html_file)
+
+	def change_extension(self,file_name, new_ext):
+		f, ext = os.path.splitext(file_name)
+		f += new_ext
+
+		return f
+
+
